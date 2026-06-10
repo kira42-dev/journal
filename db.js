9cc625587da0d753dd5dea18f353d3d761e64b1d
Developer <developer@college-journal.local>
2026-06-10 19:38:33 +0300

    fix: store journal.db in data/ subdirectory for Docker volume compatibility

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
 const Database = require('better-sqlite3');
 const bcrypt = require('bcryptjs');
 const path = require('path');
+const fs = require('fs');
 
-const db = new Database(path.join(__dirname, 'journal.db'));
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+const db = new Database(path.join(dataDir, 'journal.db'));
 
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
